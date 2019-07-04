29d8e1d497aa7dcf63b3d6c29b60073312906551
manohar <manohar0407@gmail.com>
2019-07-04 13:39:14 +0530

    initial commit

---
+++ unit-tests/src/test/java/com/comany/UnitTest.java
@@ -0,0 +1,12 @@
+package com.comany;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
